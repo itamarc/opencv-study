6c7ac4f07a9561f7052b341cbab64f19444360b7
Itamar Carvalho <itamarc@gmail.com>
2024-12-10 21:59:49 -0300

    Code formatting

--- image-annotation.py
+++ image-annotation.py
@@ -1,9 +1,11 @@
 import argparse
 import cv2 as cv
 
-# -i 'img\lago-pedras.jpg' -o 'img\lago-pedras-out.jpg' -l x1 y1 x2 y2 r g b thickness
-# -i 'img\lago-pedras.jpg' -o 'img\lago-pedras-out.jpg' -c xc yc radius r g b thickness
-# -i 'img\lago-pedras.jpg' -o 'img\lago-pedras-out.jpg' -r x1 y1 x2 y2 r g b thickness
+
+# -i 'img\lago-pedras.jpg' -o 'img\lago-pedras-annot.jpg'
+#       -l x1 y1 x2 y2 r g b thickness
+#       -c xc yc radius r g b thickness
+#       -r x1 y1 x2 y2 r g b thickness
 def init():
     parser = argparse.ArgumentParser(
         description="Image annotation using OpenCV")
@@ -12,9 +14,11 @@ def init():
     parser.add_argument('-l', '--line', nargs=8, type=int,
                         help='Draw a line (args: x1 y1 x2 y2 r g b thickness)')
     parser.add_argument('-c', '--circle', nargs=7, type=int,
-                        help='Draw a circle (args: xc yc radius r g b thickness)')
+                        help='Draw a circle \
+                            (args: xc yc radius r g b thickness)')
     parser.add_argument('-r', '--rectangle', nargs=8, type=int,
-                        help='Draw a rectangle (args: x1 y1 x2 y2 r g b thickness)')
+                        help='Draw a rectangle \
+                            (args: x1 y1 x2 y2 r g b thickness)')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = vars(parser.parse_args())
     args['line_on'] = args['line'] is not None
@@ -42,17 +46,23 @@ if (conf['verbose']):
 if (conf['line_on']):
     cv.line(output_img, (conf['line'][0], conf['line'][1]),
             (conf['line'][2], conf['line'][3]),
-            (conf['line'][6], conf['line'][5], conf['line'][4]),
+            (conf['line'][6],   # B
+             conf['line'][5],   # G
+             conf['line'][4]),  # R
             conf['line'][7], lineType=cv.LINE_AA)
 if (conf['circle_on']):
     cv.circle(output_img, (conf['circle'][0], conf['circle'][1]),
-            conf['circle'][2],
-            (conf['circle'][5], conf['circle'][4], conf['circle'][3]),
-            conf['circle'][6], lineType=cv.LINE_AA)
+              conf['circle'][2],
+              (conf['circle'][5],   # B
+               conf['circle'][4],   # G
+               conf['circle'][3]),  # R
+              conf['circle'][6], lineType=cv.LINE_AA)
 if (conf['rectangle_on']):
     cv.rectangle(output_img, (conf['rectangle'][0], conf['rectangle'][1]),
-            (conf['rectangle'][2], conf['rectangle'][3]),
-            (conf['rectangle'][6], conf['rectangle'][5], conf['rectangle'][4]),
-            conf['rectangle'][7], lineType=cv.LINE_AA)
+                 (conf['rectangle'][2], conf['rectangle'][3]),
+                 (conf['rectangle'][6],     # B
+                  conf['rectangle'][5],     # G
+                  conf['rectangle'][4]),    # R
+                 conf['rectangle'][7], lineType=cv.LINE_AA)
 
 cv.imwrite(conf['output'], output_img)
